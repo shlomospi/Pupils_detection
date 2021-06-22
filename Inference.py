Fixed bugs, updated requirements.txt and gitignore, added averaging prediction option.

Inference.py
@@ -24,14 +24,11 @@ root.attributes('-topmost', True)
 
 def parse_args():
 
-    parser = argparse.ArgumentParser(description="inference pupil location")
-    parser.add_argument("-i", "--image_folder", type=str, default="", help='image path')
+    parser = argparse.ArgumentParser(description="fast inference pupil location")
+    # parser.add_argument("-i", "--image_folder", type=str, default="", help='image path') # TODO
     parser.add_argument("-v", '--video', type=str, default="", help='video path')
-
-    parser.add_argument('--model',
-                        default="BatchSize_64_Epochs_200_LearningRate_001_model_small_dataRGB_phaseretrain_saved_model",
-                        help="relative path to saved model")
-    parser.add_argument("--save", type=bool, default=False, help="save the results?")
+    parser.add_argument("--save", type=bool, default=True, help="save the results?")
+    parser.add_argument("-a", "--average", type=int, default=1, help="How many frames back to average prediction")
     return check_args(parser.parse_args())
 
 
@@ -47,6 +44,17 @@ def check_args(args):
     return args
 
 
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
 def main():
     res = (64, 32)
     args = parse_args()
@@ -58,7 +66,7 @@ def main():
     except AttributeError:
         print("The folder does not contain a saved model.\n "
               "Choose log/<training session>/final_model")
-    # model = tf.keras.models.load_model('saved_model')
+
     model.summary()
 
     if mode == "video":
@@ -73,20 +81,27 @@ def main():
         height = int(vid.get(cv.CAP_PROP_FRAME_HEIGHT))
         fps = int(vid.get(cv.CAP_PROP_FPS))
         codec = cv.VideoWriter_fourcc(*'XVID')
-        out = cv.VideoWriter("output_inference.avi", codec, fps, (width, height))
+        if args.average > 1:
+            output_video_path = os.path.join(model_path,
+                                             "output_{}_inference_avg{}.avi".format(args.video[:-4],
+                                                                                    args.average))
+        else:
+            output_video_path = os.path.join(model_path, "output_{}_inference.avi".format(args.video[:-4]))
+        out = cv.VideoWriter(output_video_path, codec, fps, (width, height))
 
         cv.namedWindow("output", cv.WINDOW_NORMAL)
         cv.resizeWindow('output', 900, 600)
 
         times = []
-
+        predictions_x = []
+        predictions_y = []
         while True:
             _, frame = vid.read()
 
             if frame is None:
-                print("Empty Frame")
-                time.sleep(0.1)
-                continue
+                # print("Empty Frame")
+                break
+
             # original_res = (vid.get(4), vid.get(3)) # x, y
 
             frame_resized = cv.resize(frame, res, fx=0, fy=0, interpolation=cv.INTER_CUBIC)
@@ -101,19 +116,23 @@ def main():
             t2 = time.time()
             times.append(t2 - t1)
             times = times[-10:]
+            if args.average > 1:
+                predictions_x.append(x)
+                predictions_y.append(y)
+                predictions_x = predictions_x[-args.average:]
+                predictions_y = predictions_y[-args.average:]
+                x = sum(predictions_x)/len(predictions_x)
+                y = sum(predictions_y)/len(predictions_y)
 
-            # coords = denormalize_coord((x, y), original_res)
-            # frame_new = cv.resize(frame_new, (900, 600), fx=0, fy=0, interpolation=cv.INTER_CUBIC)  # temp
             out_img = cross_annotator(frame, (x, y), size = frame.shape[0]//50)
-            # out_proccessed = cross_annotator(frame_new, (x, y), size=frame.shape[0] // 50)
+
             out_img = cv.putText(out_img, "FPS: {:.2f} Pred: {}".format(len(times) / sum(times), (x, y)), (0, 30),
                                  cv.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 2)
-
-            out.write(out_img)
+            if args.save:
+                out.write(out_img)
 
             cv.imshow('output', out_img)
 
-            # cv.imshow('outputp', out_proccessed)
             if cv.waitKey(1) == ord('q'):
                 break
 

Main.py
@@ -1,4 +1,3 @@
-#from tensorflow import keras
 import tensorflow as tf
 import numpy as np
 import csv
@@ -12,21 +11,11 @@ from prep_data import *
 import argparse
 from clearml import Task
 from tkinter import Tk, filedialog
-"""import imgaug as ia
-import imgaug.augmenters as iaa
-from imgaug.augmentables import Keypoint, KeypointsOnImage
-
-# ------------------------------------imgaug image augmentation init----------------------------------------------------
-
-augments = [iaa.Affine(translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
-                       rotate=(-15, 15)),
-            iaa.Fliplr(p=0.5)]
-seq = iaa.Sequential(augments)
 """
 # ----------------------------------------clear ml init-----------------------------------------------------------------
 
 task = Task.init()
-
+"""
 # ----------------------------------------tkinter init-----------------------------------------------------------------
 
 root = Tk()
@@ -41,34 +30,68 @@ print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 # ----------------------------------------parsing-----------------------------------------------------------------------
 def parse_args():
 
-    parser = argparse.ArgumentParser(description="Pupil center detection trainer")
-    parser.add_argument("-p", '--phase', type=str, default='train',
-                        help='train / retrain')
-    parser.add_argument("-e", '--epoch', type=int, default=400,
-                        help='The number of epochs to run')
-    parser.add_argument("-b", '--batch_size', type=int, default=64,
-                        help='The size of batch')
-    parser.add_argument('--lr', type=float, default=0.001,
+    parser = argparse.ArgumentParser(description="fast Pupil center detection trainer")
+    parser.add_argument("-p", '--phase',
+                        type=str,
+                        default='train',
+                        help='train / retrain. \n'
+                             'retraining will ask for a "saved_model" file and ignore any architecture choices')
+
+    parser.add_argument("-e", '--epoch',
+                        type=int,
+                        default=400,
+                        help='The maximal number of epochs to run')
+    parser.add_argument("-b", '--batch_size',
+                        type=int,
+                        default=64,
+                        help='The size of a batch')
+    parser.add_argument('--lr',
+                        type=float,
+                        default=0.001,
                         help='learning rate')
-    parser.add_argument('--log_dir', type=str, default='',
-                        help='Directory name to save training logs')
-    parser.add_argument('--blocks', default = 2,
-                        help="num of blocks")
-    parser.add_argument('--arch', default="arch",
-                        help="small/blocks")
-    parser.add_argument('-bin', '--binary', default=False, type=bool,
-                        help="True for converting data to binary pixels, Flase ")
-    parser.add_argument('--data', default="RGB",
-                        help="what dataset to load (IR/RGB/Both)")
-    parser.add_argument('--threshold', nargs='+', default=(79 // 2, 284 // 2, 0, 255, 0, 107),
-                        help=' threshold (Hmin, Hmax, Smin, Smax, Vmin, Vmax)')
+    parser.add_argument('--reducelr',
+                        type=int,
+                        default=100000,
+                        help='by how much to reduce learning rate')
+    parser.add_argument('--log_dir',
+                        type=str,
+                        default='',
+                        help='Directory name to save training logs, any pictures and saved models')
+    parser.add_argument('--blocks',
+                        default = 2,
+                        help="num of blocks for a block type architecture")
+    parser.add_argument('--arch',
+                        default="arch",
+                        help="small/blocks/medium")
+    parser.add_argument('-bin', '--binary',
+                        default=False,
+                        type=bool,
+                        help="'-bin True' for converting data to binary pixels, ignore for False ")
+    parser.add_argument('--data',
+                        default="RGB",
+                        help="what dataset to load (IR/RGB/Both/BothRGB)")
+    parser.add_argument('--threshold',
+                        nargs='+',
+                        default=(79 // 2, 284 // 2, 0, 255, 0, 107),
+                        help=' threshold (Hmin, Hmax, Smin, Smax, Vmin, Vmax) for image preproccessing')
+    parser.add_argument('--filters',
+                        nargs='+',
+                        default=(16, 32, 64),
+                        help='filters for "medium" net')
+    parser.add_argument('-a', '--augmentation',
+                        nargs='+',
+                        default=("flip"), # TODO recheck trans option
+                        help='what augmentation to do? '
+                             'flip for flipingLR trans and an int for '
+                             'translating horizontaly up to <int> '
+                             'and vertically up to 2*<int>')
     return check_args(parser.parse_args())
 
 
 def check_args(args):
     # --data
     try:
-        assert args.data == "RGB" or "IR" or "Both" or "RGB2"
+        assert args.data == "RGB" or "IR" or "Both" or "RGB2" or "BothRGB"
     except ValueError:
         print('Not valid data source')
 
@@ -89,7 +112,7 @@ def check_args(args):
     try:
         assert args.phase == "train" or "retrain"
     except ValueError:
-        print('phase args must be equal "train", "retrain" or "evaluate"')
+        print('phase args must be equal "train" or "retrain"')
     if args.phase == "retrain":
         print("Retraining a model, overwriting Architecture choice")
     # --epoch
@@ -113,7 +136,7 @@ def check_args(args):
     return args
 
 
-def main():
+def main(verbose = 2):
     #  -----------------------------------------parameters------------------------------------------------------------
     print("\n\nInit..")
     print("-----------------------------------Parsing-----------------------------------")
@@ -127,7 +150,18 @@ def main():
     print("-----------------------------Creating Log Folder-----------------------------")
 
     template = "BatchSize_{}_Epochs_{}_LearningRate_{}_model_{}_data_{}_phase_{}"
-    config = template.format(batch_size, epochs, str(learning_rate)[2:], args.arch, args.data, args.phase)
+    if args.binary:
+        template += "_binary"
+    if args.arch == "medium":
+        arch = "medium{}_{}_{}".format(args.filters[0],
+                                       args.filters[1],
+                                       args.filters[2])
+    elif args.arch == "blocks":
+        arch = "block{}".format(args.blocks)
+    else:
+        arch = args.arch
+
+    config = template.format(batch_size, epochs, str(learning_rate)[2:], arch, args.data, args.phase)
     if type(args.log_dir) is str and args.log_dir != "":
         log_folder = "log/{}".format(args.log_dir)
     else:
@@ -157,37 +191,56 @@ def main():
         RGBimages, RGBlabels = create_RGB_data(tresh=tresh, binary=args.binary, verbose=2)
         images = np.concatenate((IRimages, RGBimages), axis = 0)
         labels = np.concatenate((IRlabels, RGBlabels), axis = 0)
+
     elif args.data == "BothRGB":
         print("Loading Both RGB and RGB2 data, and " + binary_message)
         RGB2images, RGB2labels = create_RGB2_data(verbose=2, binary=args.binary)
         RGBimages, RGBlabels = create_RGB_data(verbose=2, binary=args.binary)
         images = np.concatenate((RGB2images, RGBimages), axis=0)
         labels = np.concatenate((RGB2labels, RGBlabels), axis=0)
+
     else:
         raise SystemExit("Typo is dataset name")
 
     if images.ndim == 3: # for 1 channel images
         images = np.expand_dims(images, axis=-1)
-    print("Spliting")
-    x_train, x_test, y_train, y_test = train_test_split(images, labels, test_size=0.1, random_state=42)
-    print("augmenting")
 
-    x_train, y_train = flipLR_img_landmark(x_train, y_train)
-    x_train, y_train = translate_img_landmark(x_train, y_train, max_x=8, max_y=4, iterations=4)
+    print("Spliting..")
+    x_train, x_test, y_train, y_test = train_test_split(images, labels,
+                                                        test_size=0.1, random_state=42)
+    print("augmenting..")
 
+    augmentations = args.augmentation
+    if "flip" in augmentations:
+        print("Flipping")
+        x_train, y_train = flipLR_img_landmark(x_train, y_train)
+
+    if "trans" in augmentations:
+        for aug in augmentations:
+            if type(aug) == int:
+                print("Translating by {}".format(aug))
+                max_pixel_trans = aug
+                x_train, y_train = translate_img_landmark(x_train, y_train,
+                                                          max_x=2*max_pixel_trans,
+                                                          max_y=max_pixel_trans,
+                                                          iterations=4)
+                break
+    print("splitting valset from testset")
     x_val, x_test, y_val, y_test = train_test_split(x_test, y_test, test_size=0.4, random_state=7)
+    print("final datasets sizes:")
     print('Train data size: {}, train label size: {}'.format(x_train.shape, y_train.shape))
     print('val data size:   {}, val label size:   {}'.format(x_val.shape, y_val.shape))
     print('test data size:  {}, test label size:  {}'.format(x_test.shape, y_test.shape))
-
-    image_utils.plot_example_images(x_train, y_train,
-                                    title="examples from the training dataset")
+    if verbose > 2:
+        image_utils.plot_example_images(x_train, y_train,
+                                        title="examples from the training dataset")
 
     # -------------------------------------Load & compile model---------------------------------------------------------
     print("----------------------------Loading & Compiling Model----------------------------------")
 
     if args.phase == "retrain":
-        model_path = filedialog.askdirectory() # Choose
+
+        model_path = filedialog.askdirectory() # prompt user to choose a folder from which a saved model will be loaded
         try:
             model = tf.keras.models.load_model(model_path)
             model.summary()
@@ -210,7 +263,7 @@ def main():
             model = models.CNN_small_regression(input_shape=x_train[4].shape)
             print("Loaded {} model".format(args.arch))
         elif args.arch == "medium":
-            model = models.CNN_medium_regression(input_shape=x_train[4].shape)
+            model = models.CNN_medium_regression(input_shape=x_train[4].shape, filters=args.filters)
             print("Loaded {} model".format(args.arch))
         else:
             raise SystemExit("no model found", args.arch, type(args.arch))
@@ -228,7 +281,7 @@ def main():
     #                                              write_images=True, write_graph=False)
 
     adaptivelr = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.15, patience=3,
-                                                      verbose=0, mode='auto', cooldown=2, min_lr=learning_rate//100000)
+                                                      verbose=0, mode='auto', cooldown=2, min_lr=learning_rate//args.reducelr)
 
     earlystopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=10,
                                                      verbose=0, mode='auto', baseline=None, restore_best_weights=True)
@@ -246,15 +299,10 @@ def main():
                         validation_data=(x_val, y_val),
                         verbose=2,
                         callbacks=callbacks)
-    final_model_path = os.path.join(log_folder, "final_model")
-    print(f"Saving best result at {final_model_path}")
-    model.save(final_model_path)
-    """
-    # ----------------------------------Load "saved_model"-------------------------------------------------------------
-    if not train: # TODO remove
-        print("Loading saved_model")
-        model = keras.models.load_model('saved_model')
-    """
+    # final_model_path = os.path.join(log_folder, "final_model")
+    # print(f"Saving best result at {final_model_path}")
+    # model.save(final_model_path)
+    # not needed, best model already saved by checkpointer
     #  ---------------------------------------Evaluate the model ------------------------------------------------------
     print("--------------------------------Evaluating Model---------------------------------------")
 
@@ -268,16 +316,26 @@ def main():
 
     utils.plot_acc_lss(history, metric1=loss, metric2=loss, log_dir=log_folder)
     image_utils.plot_example_images(x_test, model.predict(x_test),
-                                    title="examples of model's predictions after training",
+                                    title="examples of model's predictions after training on testset",
                                     examples=10, folder=log_folder)
-
+    if verbose > 2:
+        image_utils.plot_example_images(x_val, model.predict(x_val),
+                                        title="examples of model's predictions after training on valset",
+                                        examples=10, folder=log_folder)
+        image_utils.plot_example_images(x_train, model.predict(x_train),
+                                        title="examples of model's predictions after training on trainingset",
+                                        examples=10, folder=log_folder)
     if epochs > 2:
         print("-------------------------------Logging Experiment--------------------------------------")
         csv_path = os.path.join(os.path.dirname(os.path.realpath('__file__')),
                                 'experiment_results.csv')
         print("saveing config and results at: \n{}".format(csv_path))
-        arch = "block{}".format(args.blocks) if args.arch == "blocks" else args.arch
-        csv_line = [batch_size, epochs, learning_rate, arch, test_mse, args.data, args.phase]
+        # arch = "block{}".format(args.blocks) if args.arch == "blocks" else args.arch
+        pixel_format = "binary" if args.binary else "grayscale"
+        aug_log = ""
+        for augmentation in augmentations:
+            aug_log += str(augmentation)+"_"
+        csv_line = [batch_size, epochs, learning_rate, arch, test_mse, args.data, args.phase, pixel_format, aug_log]
 
         with open(csv_path, "a", newline='') as csvfile:
             csvwriter = csv.writer(csvfile)
@@ -288,5 +346,3 @@ def main():
 if __name__ == '__main__':
 
     main()
-
-

models.py
@@ -142,7 +142,7 @@ def CNN_medium_regression(input_shape, filters=(16, 32, 64), l2_weight_regulaize
     :param kernel:
     :return:
     """
-    # TODO test
+
     inputs = tf.keras.layers.Input(input_shape)
     x = inputs
     print("Building medium CNN regression model")

prep_data.py
@@ -13,7 +13,7 @@ import image_utils
 def video_csv_to_np_wrapper(data_path = "", txt_file = "1.txt", video_file = "1.avi",
                             tresh=(79//2, 284//2, 0, 255, 0, 107), binary=False):
     """
-
+    wrapper for the video_csv_to_np function, creates full path and asserts files are in path
     :param binary:
     :param tresh:
     :param data_path:q
@@ -38,15 +38,15 @@ def video_csv_to_np_wrapper(data_path = "", txt_file = "1.txt", video_file = "1.
 def video_csv_to_np(videopath, csvpath, res=(64, 32), augment=False,
                     tresh=(79//2, 284//2, 0, 255, 0, 107), binary=False, verbose=0):
     """
-
-    :param augment:
-    :param binary:
+    creates a dataset from a video and a csv file.
+    :param videopath: path to video
     :param verbose:
-    :param tresh:
-    :param videopath:
-    :param csvpath:
-    :param res:
-    :return:
+    :param res: final resolution for images
+    :param csvpath: path to CSV file
+    :param augment: Flase for normalization of landmarks
+    :param tresh: treshold limits
+    :param binary: True for binary pixels, false for grayscale
+    :return: y_dataset, x_dataset
     """
     cap = cv.VideoCapture(videopath)
     original_res = cap.get(3), cap.get(4)
@@ -80,8 +80,8 @@ def video_csv_to_np(videopath, csvpath, res=(64, 32), augment=False,
 
         if ret:
 
-            array_list.append(img_preprocess(frame, res=res,
-                                             tresh=tresh, binary=binary))
+            array_list.append(img_preproccess(frame, res=res,
+                                              tresh=tresh, binary=binary))
         else:
             break
 
@@ -96,16 +96,20 @@ def video_csv_to_np(videopath, csvpath, res=(64, 32), augment=False,
 def create_ir_data(tresh=(79//2, 284//2, 0, 255, 0, 107), binary=False,
                    verbose=2):
     """
-
-    :return:
+    creates a dataset from videos and a csv file in the "IR videos" directory.
+    This dataset was created from 12 IR videos.
+    :param tresh: treshold limits
+    :param binary: True for binary pixels, false for grayscale
+    :param verbose:
+    :return: x_dataset, y_dataset
     """
     label_mat = []
     img_mat = []
-    for vid in [1, 2, 3, 4, 5, 9, 13, 21, 22, 91, 111, 121]:
+    for vid in tqdm([1, 2, 3, 4, 5, 9, 13, 21, 22, 91, 111, 121]):
         txt_file = str(vid)+".txt"
         video_file = str(vid)+".avi"
 
-        labels_, images = video_csv_to_np_wrapper(data_path = "dsp-ip/data/images/IR videos/",
+        labels_, images = video_csv_to_np_wrapper(data_path = "IR videos/",
                                                   txt_file=txt_file, video_file=video_file,
                                                   tresh=tresh, binary=binary)
         label_mat.append(labels_)
@@ -122,19 +126,47 @@ def create_ir_data(tresh=(79//2, 284//2, 0, 255, 0, 107), binary=False,
 
 def create_RGB_data(res=(64, 32), normalize=True, tresh=(79//2, 284//2, 0, 255, 0, 107),
                     binary=False, verbose=0):
-
-    return img_txt_to_np("inferno/images_dataset", res=res, normalize=normalize,
+    """
+        creates a dataset from images and a csv file in the "images_dataset" directory.
+        This dataset was created by inference of the inferno_labeler.py script on images.
+        :param res: x,y final resolution
+        :param normalize: Should the landmarks be normalized
+        :param tresh: treshold limits
+        :param binary: True for binary pixels, False for grayscale
+        :param verbose:
+        :return: x_dataset, y_dataset
+        """
+    return img_txt_to_np("images_dataset", res=res, normalize=normalize,
                          binary=binary, tresh=tresh, verbose=verbose)
 
 
 def create_RGB2_data(res=(64, 32), normalize=True, tresh=(79//2, 284//2, 0, 255, 0, 107),
                      binary=False, verbose=0):
+    """
+    creates a dataset from images and a csv file in the "images_from_videos1_dataset" directory.
+    This dataset was created by inference of the inferno_labeler.py script on 5 videos.
+    :param res: x,y final resolution
+    :param normalize: Should the landmarks be normalized
+    :param tresh: treshold limits
+    :param binary: True for binary pixels, False for grayscale
+    :param verbose: 
+    :return: x_dataset, y_dataset
+    """
 
-    return img_txt_to_np("inferno/images_from_videos1_dataset", res=res, normalize=normalize,
+    return img_txt_to_np("images_from_videos1_dataset", res=res, normalize=normalize,
                          binary=binary, tresh=tresh, verbose=verbose)
 
 
-def img_preprocess(image, res=(64, 32), tresh=(79//2, 284//2, 0, 255, 0, 107), binary=False):
+def img_preproccess(image, res=(64, 32), tresh=(79 // 2, 284 // 2, 0, 255, 0, 107), binary=False):
+    """
+    Takes and image or frame, reduces the resolution, converts to HSV, preforms tresholding,
+    and converts to grayscale or binary pixels.
+    :param image: image or frame
+    :param res: x,y final shape
+    :param tresh: treshold limits
+    :param binary: blooean, true for binary pixels, false for grayscale
+    :return: proccessed image
+    """
     low_H, high_H, low_S,  high_S, low_V, high_V = tresh
     resized_frame = cv.resize(image, res, fx=0, fy=0, interpolation=cv.INTER_CUBIC)
     frame_HSV = cv.cvtColor(resized_frame, cv.COLOR_BGR2HSV)
@@ -151,7 +183,8 @@ def img_preprocess(image, res=(64, 32), tresh=(79//2, 284//2, 0, 255, 0, 107), b
 def img_txt_to_np(folderpath, res=(64, 32), tresh=(79//2, 284//2, 0, 255, 0, 107),
                   normalize=True, binary=False, verbose=0):
     """
-
+    reads jpg images from a folder and the landmark form a CSV file (each line number coresponds to the image number,
+    with the coordiantes as two floats separated by " ". creates from them a dataset of the shapes (N,H,W,1), (N,2)
     :param binary: convert to binary or leave in grayscale
     :param tresh: treshhold to use on HSV format
     :param folderpath:
@@ -185,14 +218,7 @@ def img_txt_to_np(folderpath, res=(64, 32), tresh=(79//2, 284//2, 0, 255, 0, 107
         image = cv.imread(image_path, cv.IMREAD_COLOR)
 
         # img handling
-        """frame = cv.resize(image, res, fx=0, fy=0, interpolation=cv.INTER_CUBIC)
-        frame_HSV = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-        frame_threshold = cv.inRange(frame_HSV, (low_H, low_S, low_V), (high_H, high_S, high_V))
-        frame_new = cv.bitwise_and(frame_HSV, frame_HSV, mask=frame_threshold)
-        frame_new = frame_new[:, :, 0]
-        frame_array = np.asarray(frame_new)
-        array_list.append(frame_array)"""
-        array_list.append(img_preprocess(image, res=res, tresh=tresh, binary=binary))
+        array_list.append(img_preproccess(image, res=res, tresh=tresh, binary=binary))
         # label handling
         label_line = utils.read_selected_line(labelpath, image_num)
         # original_res = image.shape[:2] # y, x
@@ -215,7 +241,12 @@ def img_txt_to_np(folderpath, res=(64, 32), tresh=(79//2, 284//2, 0, 255, 0, 107
     return array_list, label_list
 
 
-def view_img_dataset(folderpath):
+def view_img_dataset(folderpath="images_dataset"):
+    """
+    iterates over a folder with numbered jpg images and shows them on screen with the landmark on them
+    :param folderpath:
+    :return:
+    """
     fileDir = os.path.dirname(os.path.realpath('__file__'))
     path = os.path.join(fileDir, folderpath)
     labelpath = os.path.join(path, "labels.txt")
@@ -246,14 +277,15 @@ def view_img_dataset(folderpath):
 
 def flipLR_img_landmark(x_train, y_train):
     """
-    flips and adds flipped images to dataset with one landmark
-    :param x_train:
-    :param y_train:
-    :return:
+    flips and adds flipped images to input dataset with one landmark
+    :param x_train: images, nparray of shape (N,H,W,1)
+    :param y_train: landmarks, nparray of shape (N,2)
+    :return: x_train, y_train, with flipped version appended
     """
     if y_train[0, 0] <= 1 and y_train[0, 1] <= 1:
         x_flipedlr = tf.image.flip_left_right(x_train)
-        y_flipedlr = np.subtract(1, y_train)
+        y_flipedlr = np.subtract([[1, 0]], y_train)
+        y_flipedlr = np.multiply([[1, -1]], y_flipedlr)
         x_train = np.concatenate((x_train, x_flipedlr), axis=0)
         y_train = np.concatenate((y_train, y_flipedlr), axis=0)
 
@@ -265,6 +297,15 @@ def flipLR_img_landmark(x_train, y_train):
 
 
 def translate_img_landmark_once(x_train, y_train, t_x=3, t_y=2):
+    """
+    takes a dataset as nparray of shape (N,H,W,1) and landmarks GT of shape (N,2).
+    Creates an augmentation of each image as a new dataset which is returned.
+    :param x_train: images, nparray of shape (N,H,W,1)
+    :param y_train: landmarks, nparray of shape (N,2)
+    :param t_x: horizontal translation distrance
+    :param t_y: vertical translation distrance
+    :return: augmented dataset
+    """
     assert x_train.ndim == 4
 
     img_shape = x_train.shape
@@ -295,6 +336,18 @@ def translate_img_landmark_once(x_train, y_train, t_x=3, t_y=2):
 
 
 def translate_img_landmark(x_train, y_train, max_x=8, max_y=2, iterations=4):
+    """
+    takes a dataset as nparray of shape (N,H,W,1) and landmarks GT of shape (N,2).
+    Creates <iterations> augmentations of each image and
+    appends them to the input as one dataset which is returned.
+
+    :param x_train: nparray of shape (N,H,W,1)
+    :param y_train: nparray of shape (N,2)
+    :param max_x: limit of horizontal translation, by pixels
+    :param max_y: limit of vertical translation, by pixels
+    :param iterations: how many augmentation per image to make
+    :return:
+    """
 
     assert x_train.ndim == 4
 